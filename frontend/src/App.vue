e3dec12a0958579edaf55b283f8e27e033bd5d70
Chandra <146106387+lzch4nnn@users.noreply.github.com>
2026-07-01 22:53:04 +0800

    restore: kembalikan folder frontend yang terhapus tidak sengaja
    Folder frontend/ (dashboard Vue 3) sempat terhapus saat commit
    be0732b "preprocessing sistem". PR ini mengembalikan isinya persis
    seperti commit 3be234c tanpa mengubah perubahan lain di main.

---
+++ frontend/src/App.vue
@@ -0,0 +1,12 @@
+<script setup>
+import NavBar from './components/NavBar.vue'
+import FooterBar from './components/FooterBar.vue'
+</script>
+
+<template>
+  <NavBar />
+  <main class="page-grow">
+    <RouterView />
+  </main>
+  <FooterBar />
+</template>
